971e0c387f4018cd614b6784f211600b020e7e24
CJJiang <37054767+CJJiang@users.noreply.github.com>
2018-03-29 20:32:27 +0800

    0326作業

---
+++ 0326/main.py
@@ -0,0 +1,4 @@
+from source import calculator
+x=int(input("please enter num1:"))
+y=int(input("please enter num2:"))
+calculator.cal(x,y)
---
+++ 0326/source/calculator.py
@@ -0,0 +1,12 @@
+def cal(arg1,arg2):
+    z=input("select +,-,*,/:")
+    if z=="+":
+         print(arg1+arg2)
+    elif z=="-":
+         print(arg1-arg2)
+    elif z=="*":
+         print(arg1*arg2)
+    elif z=="/":
+         print(arg1/arg2)
+    else:
+         print("error")
