822a98a5ca0d2fa4600b2b3ecfabfd635182f2ea
CJJiang <37054767+CJJiang@users.noreply.github.com>
2018-04-22 15:01:45 +0800

    123

--- 0416/0416/mysite/cj/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-
-from .models import Person
-
-# Create your views here.
-def index(request):
-	person=Person.objects.all()
-	return render_to_response('cj/introduction.html', locals())
